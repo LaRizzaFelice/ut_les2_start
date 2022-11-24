3b8f576857132609a4c296c0ed58c3f9119989fa
Felice La Rizza <larizza.felice@gmail.com>
2022-11-24 09:20:20 +0100

    Initial commit

---
+++ src/main/java/ex05_arrays_OBF/ArrayOfInts.java
@@ -0,0 +1,25 @@
+package ex05_arrays_OBF;
+
+public class ArrayOfInts {
+
+    public int sumOfArray(int[] array) {
+        if (array == null) return 0;
+
+        int sum = 0;
+
+        for (int i : array) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public int sumOfEvenNumbers(int[] array) {
+        if (array == null) return 0;
+        int getal = 0;
+
+        for (int element : array )
+            if (element % 2 == 0) getal += element;
+        return getal;
+    }
+
+}
